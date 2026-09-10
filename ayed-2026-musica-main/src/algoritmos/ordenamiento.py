b571ecb81b30b1b7ecfd6ff027878ffb635cf7d8
lautaroarielaltamirano-ctrl <lautaroarielaltamirano@gmail.com>
2026-09-09 22:55:27 -0300

    Delete ayed-2026-musica-main directory

--- ayed-2026-musica-main/src/algoritmos/ordenamiento.py
+++
@@ -1,8 +0,0 @@
-def ordenar_seleccion(coleccion, clave):
-    """Ordenamiento por selección. Devuelve una nueva secuencia, no usa sorted."""
-    raise NotImplementedError
-
-
-def ordenar_insercion(coleccion, clave):
-    """Ordenamiento por inserción. Devuelve una nueva secuencia, no usa sorted."""
-    raise NotImplementedError
--- ayed-2026-musica-main/src/tads/nodo.py
+++
@@ -1,4 +0,0 @@
-class Nodo:
-    def __init__(self, dato, siguiente=None):
-        self.dato = dato
-        self.siguiente = siguiente
